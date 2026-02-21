bdf9ca70b5036aedbe9819370c870cb828bd11df
|GriB28| <gp.off.dev@gmail.com>
2026-02-21 22:49:23 +0300

    [tb-6] полный функционал настройки аватарки + фиксы

--- source/store/settings.py
+++ source/store/settings.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 from fastapi.responses import JSONResponse, FileResponse
-from os.path import getmtime
 
-from scripts import lpsql, parser
+from os.path import getmtime, exists
+
+from scripts import lpsql, parser, memory
 from data import config as cfg
 
 
@@ -12,19 +13,22 @@ db = lpsql.DataBase(cfg.PATHS.DATA + "lypay_database.db", lpsql.Tables.MAIN)
 
 @router.get("/avatar/get")
 async def get_avatar(ID: int = None, unix: float = None):
-    if ID is None or unix is None:
+    if ID is None:
         return parser.form_error_bad_parsing()
 
     try:
+        path = cfg.PATHS.STORES_AVATARS + f"{ID}.jpg"
         has_icon = db.search("stores", "ID", ID)["logo"]
-        if has_icon is None:
+
+        if not bool(has_icon):
             return JSONResponse(
                 {"result": "no icon"},
                 status_code=200
             )
 
-        path = cfg.PATHS.STORES_AVATARS + f"{ID}.jpg"
-        if unix <= getmtime(path):
+        if not exists(path):
+            return parser.form_error(FileNotFoundError(), "avatar not found", 404)
+        if unix is not None and unix >= getmtime(path):
             return JSONResponse(
                 {"result": "avatar didn't change"},
                 status_code=200
@@ -32,9 +36,29 @@ async def get_avatar(ID: int = None, unix: float = None):
 
         return FileResponse(
             path,
+            media_type='image/jpg',
             status_code=200
         )
     except lpsql.exceptions.IDNotFound as e:
         return parser.form_error(e, "ID not found", 404)
+    except Exception as e:
+        return parser.form_error(e)
+
+
+@router.post("/avatar/set")
+async def set_avatar(avatar: UploadFile, ID: int = None):
+    if ID is None:
+        return parser.form_error_bad_parsing()
+
+    try:
+        has_icon = db.search("stores", "ID", ID)["logo"]
+        if not bool(has_icon):
+            db.update("stores", "ID", ID, "logo", True)
+
+            await memory.save_iterative(avatar, cfg.PATHS.STORES_AVATARS + f"{ID}.jpg")
+            return JSONResponse(
+                {"ok": True},
+                status_code=200
+            )
     except Exception as e:
         return parser.form_error(e)
