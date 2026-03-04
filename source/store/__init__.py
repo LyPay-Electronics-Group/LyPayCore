93927487013cfeea879f4fe58ad82fd0481dd897
|GriB28| <gp.off.dev@gmail.com>
2026-03-04 22:31:54 +0300

    [tb-7] роутер для взаимодействия с товарами, функция добавления нового

--- source/store/__init__.py
+++ source/store/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .settings import router as settings_router
+from .items import router as items_router
 
 
 router = APIRouter()
 
 router.include_router(settings_router, prefix="/settings")
+router.include_router(items_router, prefix="/items")
---
+++ source/store/items.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+from scripts import lpsql, parser
+from data import config as cfg
+
+
+router = APIRouter()
+db = lpsql.DataBase(cfg.PATHS.DATA + "lypay_database.db", lpsql.Tables.MAIN)
+
+
+@router.get("/add")
+async def create_item(storeID: str = None, name: str = None, price: int = None):
+    if storeID is None or name is None or price is None:
+        return parser.form_error_bad_parsing()
+
+    try:
+        if storeID not in db.searchall("stores", "ID"):
+            raise lpsql.exceptions.IDNotFound
+
+        itemID = f"{storeID}_{parser.generate_code(6)}"
+        while itemID in db.searchall("items", "itemID"):
+            itemID = f"{storeID}_{parser.generate_code(6)}"
+
+        db.insert("items", [
+            itemID,
+            storeID,
+            name,
+            price,
+            True    # active flag
+        ])
+        return JSONResponse(
+            {'ok': True},
+            status_code=201
+        )
+    except lpsql.exceptions.IDNotFound as e:
+        return parser.form_error(e, "ID not found", 404)
+    except Exception as e:
+        return parser.form_error(e)
