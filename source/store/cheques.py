53ee2edc890533d13c3414ff90bb9e8fe9459eba
|GriB28| <gp.off.dev@gmail.com>
2026-03-14 12:05:11 +0300

    [tb-9] завершённый роутер чеков + интеграция генератора ID

--- source/store/cheques.py
+++ source/store/cheques.py
@@ -1,14 +1,22 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from asyncio import sleep
+from jwt import decode as jwt_decode
 
-from scripts import lpsql, parser, idgen
+from dotenv import load_dotenv
+from os import getenv
+
+from scripts import lpsql, parser
+from scripts.idgen import IDGenerator
+from scripts.unix import unix
 from data import config as cfg
 
 
 router = APIRouter()
 db = lpsql.DataBase(cfg.PATHS.DATA + "lypay_database.db", lpsql.Tables.MAIN)
+idgen = IDGenerator(db)
+
+load_dotenv()
 
 
 @router.get("/get")
@@ -51,6 +59,54 @@ async def get_all_cheques(storeID: str = None, active_filter: bool = None):
             status_code=200
         )
     except lpsql.exceptions.IDNotFound as e:
+        return parser.form_error(e, "ID not found", 404)
+    except Exception as e:
+        return parser.form_error(e)
+
+
+@router.get("/add")
+async def create_cheque(storeID: str = None, customer: int = None, values: str = None):
+    if storeID is None or customer is None or values is None:
+        return parser.form_error_bad_parsing()
+
+    try:
+        if storeID not in db.searchall("stores", "ID"):
+            raise lpsql.exceptions.IDNotFound
+
+        parsed_values = jwt_decode(values, getenv("LYPAY_JWT"), algorithm="HS256")
+        chequeID = await idgen.chequeID(storeID)
+
+        db.insert("cheques", [
+            chequeID,
+            storeID,
+            unix(),
+            customer,
+            parsed_values,
+            True  # active flag
+        ])
+        return JSONResponse(
+            {'generated': chequeID},
+            status_code=201
+        )
+    except lpsql.exceptions.IDNotFound as e:
+        return parser.form_error(e, "ID not found", 404)
+    except Exception as e:
+        return parser.form_error(e)
+
+
+@router.get("/de")
+async def deactivate_cheque(chequeID: str = None):
+    if chequeID is None:
+        return parser.form_error_bad_parsing()
+
+    try:
+        db.update("cheques", "chequeID", chequeID, "active", False)
+
+        return JSONResponse(
+            {'ok': True},
+            status_code=200
+        )
+    except lpsql.exceptions.EntryNotFound as e:
         return parser.form_error(e, "ID not found", 404)
     except Exception as e:
         return parser.form_error(e)
