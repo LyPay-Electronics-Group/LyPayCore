dc3120e5fae242079f3a4a40eff7089f7fc3779f
|GriB28| <gp.off.dev@gmail.com>
2026-03-21 00:01:22 +0300

    [tb-12] функция проверки статуса агента

--- source/admin/agent.py
+++ source/admin/agent.py
@@ -7,6 +7,27 @@ from data import config as cfg
 
 router = APIRouter()
 db = lpsql.DataBase(cfg.PATHS.DATA + "lypay_database.db", lpsql.Tables.MAIN)
+firewall4 = lpsql.DataBase(cfg.PATHS.DATA + "lypay_firewall.db", lpsql.Tables.FIREWALL)
+
+
+@router.get("/check")
+async def check_agent_status(userID: int = None):
+    if userID is None:
+        return parser.form_error_bad_parsing()
+
+    try:
+        result = firewall4.search("admins", "ID", userID)
+        if result is None:
+            raise lpsql.exceptions.IDNotFound
+
+        return JSONResponse(
+            {'result': result},
+            status_code=200
+        )
+    except lpsql.exceptions.IDNotFound as e:
+        return parser.form_error(e, "ID not found", 404)
+    except Exception as e:
+        return parser.form_error(e)
 
 
 @router.get("/deposit")
