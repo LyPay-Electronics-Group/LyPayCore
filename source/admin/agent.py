7c431f6ca28dd34d69e57994755e6cf7ef7c8836
|GriB28| <gp.off.dev@gmail.com>
2026-03-20 23:55:13 +0300

    [tb-12] функция агентского пополнения

--- source/admin/agent.py
+++ source/admin/agent.py
@@ -10,5 +10,18 @@ db = lpsql.DataBase(cfg.PATHS.DATA + "lypay_database.db", lpsql.Tables.MAIN)
 
 
 @router.get("/deposit")
-async def do_agent_deposit():
-    pass
+async def do_agent_deposit(userID: int = None, amount: int = None, agentID: int = None):
+    if userID is None or amount is None or agentID is None:
+        return parser.form_error_bad_parsing()
+
+    try:
+        db.deposit(userID, amount, agentID)
+
+        return JSONResponse(
+            {'ok': True},
+            status_code=200
+        )
+    except lpsql.exceptions.IDNotFound as e:
+        return parser.form_error(e, "ID not found", 404)
+    except Exception as e:
+        return parser.form_error(e)
