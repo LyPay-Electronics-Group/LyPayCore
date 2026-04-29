5b9377c741e8dd0914ee16505ae25df3cb9d139f
|GriB28| <gp.off.dev@gmail.com>
2026-04-29 20:19:48 +0300

    [mst-1] третий тест

--- source/mst/__init__.py
+++ source/mst/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .test1 import router as test1_router
+from .test3 import router as test3_router
 
 
 router = APIRouter()
 
 router.include_router(test1_router)
+router.include_router(test3_router)
