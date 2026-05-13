d3d444b8d756913395e60e783be187febff5b5ce
|GriB28| <gp.off.dev@gmail.com>
2026-05-13 21:07:04 +0300

    [b-24] роутер оплаты FPS добавлен в сборку

--- source/fps/__init__.py
+++ source/fps/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .info import router as info_router
 from .access import router as access_router
+from .payment import router as payment_router
 
 
 router = APIRouter()
 
 router.include_router(info_router)
 router.include_router(access_router)
+router.include_router(payment_router)
