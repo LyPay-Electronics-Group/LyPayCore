225f7740598d66b699d8a6d7a6e1c729840d78f7
|GriB28| <gp.off.dev@gmail.com>
2026-08-21 04:38:09 +0300

    [b-27] новый драйвер БД (attempt 2)

--- source/promo.py
+++ source/promo.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends as D
 from fastapi.responses import JSONResponse
 
-from scripts import lpsql, parser
+from scripts import parser
 from scripts.token_validator import token_validate_factory as TVF
-from data.config import PATHS, TOKENIZER
+from data.config import TOKENIZER
+import database as db
 
 
 router = APIRouter()
-db = lpsql.DataBase(PATHS.DATA + "lypay_database.db", lpsql.Tables.MAIN)
 
 
 @router.get("/all")
@@ -15,10 +15,15 @@ async def get_all(
         _ = D(TVF(*TOKENIZER.ADMIN_LIST))
 ):
     try:
-        return JSONResponse(
-            {'all': db.get_table("promo")},
-            status_code=200
-        )
+        async with db.session_link() as session:
+            return JSONResponse(
+                {
+                    'all': [dict(t) for t in (await session.execute(
+                        db.select(db.Promo).where(db.Promo.active.is_(True))
+                    )).mappings().all()]
+                },
+                status_code=200
+            )
     except Exception as e:
         return parser.form_error(e)
 
@@ -31,13 +36,15 @@ async def get(
     if ID is None:
         return parser.form_error_bad_parsing()
 
+    ID = ID.lower()
     try:
-        ID = ID.lower()
-        record = db.search("promo", "ID", ID)
-        if record is None:
-            raise db.exceptions.NotFound
+        async with db.session_link() as session:
+            record = await session.get(db.Promo, ID)
+            if record is None:
+                raise db.exceptions.NotFound
+
         return JSONResponse(
-            record,
+            record.as_dict(),
             status_code=200
         )
     except db.exceptions.NotFound as e:
@@ -56,15 +63,22 @@ async def add(
     if ID is None or value is None or author is None:
         return parser.form_error_bad_parsing()
 
+    ID = ID.lower()
     try:
-        record = db.search("promo", "ID", ID.lower())
-        if record is not None:
-            return JSONResponse(
-                {"error": "ID already exists", "message": "ID already exists"},
-                status_code=409
-            )
+        async with db.session_link() as session:
+            async with session.begin():
+                if (await session.get(db.Promo, ID)) is not None:
+                    return JSONResponse(
+                        {"error": "ID already exists", "message": "ID already exists"},
+                        status_code=409
+                    )
+                session.add(db.Promo(
+                    ID=ID,
+                    value=int(value),
+                    author=author,
+                    active=True
+                ))
 
-        db.manual(f"INSERT INTO promo (ID, value, author, active) VALUES ('{ID}', {int(value)}, '{author}', 1)")
         return JSONResponse(
             {'ok': True},
             status_code=200
@@ -84,21 +98,27 @@ async def edit(
     if ID is None or not any((value, author, active)):
         return parser.form_error_bad_parsing()
 
+    ID = ID.lower()
     try:
-        ID = ID.lower()
-        if value is not None:
-            db.update("promo", "ID", ID, "value", int(value))
-        if author is not None:
-            db.update("promo", "ID", ID, "author", author)
-        if active is not None:
-            active = active.lower()
-            active = True if active == "true" else active
-            active = False if active == "false" else active
-            active = active if type(active) is bool else int(active)
-            db.update("promo", "ID", ID, "active", bool(active))
+        async with db.session_link() as session:
+            async with session.begin():
+                promo = await session.get(db.Promo, ID, with_for_update=True)
+                if value is not None:
+                    promo.value = int(value)
+                if author is not None:
+                    promo.author = author
+                if active is not None:
+                    active = active.lower()
+                    active = True if active == "true" else active
+                    active = False if active == "false" else active
+                    active = active if type(active) is bool else int(active)
+                    promo.active = bool(active)
+
         return JSONResponse(
             {'ok': True},
             status_code=200
         )
+    except ValueError:
+        return parser.form_error_bad_parsing()
     except Exception as e:
         return parser.form_error(e)
